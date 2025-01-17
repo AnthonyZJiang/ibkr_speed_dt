02d1d85e0cf8b164721800638b630041256a00a2
Zhengyi Jiang <anthony.jiang.github@outlook.com>
2025-01-17 21:51:09 +0000

    fixed a bug in sell order fill status recording

--- ibkr_speed_dt/tws_app/datastore/accountentry.py
+++ ibkr_speed_dt/tws_app/datastore/accountentry.py
@@ -1,35 +1,38 @@
-from .order import Order, OrderStatus, OrderAction
+from .order import Order, OrderAction, OrderStatus
+
 
 class TWSAccountEntry():
     
     def __init__(self, quantity: int = 0):
         self.quantity = quantity
-        self.quantity_in_sell_orders = 0
         self.open_sell_orders: dict[int, int] = {} # order_id: quantity
         
     @property
     def projected_quantity(self):
-        return self.quantity - self.quantity_in_sell_orders
+        qty_in_sell_orders = sum(self.open_sell_orders.values())
+        return self.quantity - qty_in_sell_orders
     
     def update_sell_order(self, order: Order):
         if order.action != OrderAction.SELL:
             return
-        if order.id not in self.open_sell_orders:
-            self._add_sell_order(order)
+        if order.status == OrderStatus.SUBMITTED or order.status == OrderStatus.PRE_SUBMITTED:
+            if order.id not in self.open_sell_orders:
+                self._add_sell_order(order)
+            else:
+                # Partial fill
+                self.open_sell_orders[order.id] = order.unfilled
             return
-        self.quantity_in_sell_orders -= self.open_sell_orders[order.id]
-        if order.status == OrderStatus.FILLED:
+        if order.status == OrderStatus.FILLED or order.status == OrderStatus.CANCELLED or order.status == OrderStatus.INACTIVE:
+            if order.id not in self.open_sell_orders:
+                # Probably an active order that was not submitted in this session
+                return
             del self.open_sell_orders[order.id]
-        elif order.status == OrderStatus.CANCELLED:
-            self.quantity_in_sell_orders -= order.unfilled
-            del self.open_sell_orders[order.id]
+        elif order.status == OrderStatus.PENDING_CANCEL or order.status == OrderStatus.PENDING_SUBMIT or order.status == OrderStatus.API_CANCELLED:
+            return
         else:
-            unfilled = order.unfilled
-            self.open_sell_orders[order.id] = unfilled
-            self.quantity_in_sell_orders += unfilled
+            raise ValueError(f"Unexpected order status: {order.status}")
         
     def _add_sell_order(self, order: Order):
         if order.status == OrderStatus.FILLED or order.status == OrderStatus.CANCELLED or order.status == OrderStatus.INACTIVE:
             return
         self.open_sell_orders[order.id] = order.unfilled
-        self.quantity_in_sell_orders += order.unfilled
